11d0fa8416ee4f901205d968b9c0a336324182c3
Amal44b <amal4488vvvv@gmail.com>
2024-04-21 10:41:22 +0300

    Initial Commit

---
+++ MissingStuff/MissingStuffApp.swift
@@ -0,0 +1,17 @@
+//
+//  MissingStuffApp.swift
+//  MissingStuff
+//
+//  Created by amal on 12/10/1445 AH.
+//
+
+import SwiftUI
+
+@main
+struct MissingStuffApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
